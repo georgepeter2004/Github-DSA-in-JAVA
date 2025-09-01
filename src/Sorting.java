2f146b13ed6ec174d971baec0a359d3fd9e7c70f
georgepeter2004 <georgepeter200424@gmail.com>
2025-09-01 23:49:01 +0530

    update

--- src/Sorting.java
+++ src/Sorting.java
@@ -33,14 +33,13 @@ public class Sorting {
 
 
     public static void Insertionsort(int arr[]){
-        for(int i=0;i<arr.length;i++){
+        for(int i=1;i<arr.length;i++){
             for(int j=i;j>0;j--){
                 if(arr[j]<arr[j-1]){
                     int temp = arr[j];
                     arr[j]=arr[j-1];
                     arr[j-1]=temp;
                 }
-                else break;
             }
         }
         System.out.println(Arrays.toString(arr));
